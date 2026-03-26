282fb6733d1ee892235b848e222b6d06499e063d
Mahliyo <gasandigital13@gmail.com>
2026-03-26 17:52:03 +0500

    8-masala if else shartlari true

--- main.js
+++ main.js
@@ -3,8 +3,8 @@
 let a = +prompt("a sonini kiriting");
 let b = +prompt("b sonini kiriting");
 let c = +prompt("c sonini kiriting");
-if ( a = b || b = c ||  ) {
-    
+if ( a = b || b = c || a = c ) {
+    alert("true")
 } else {
     
 }
