77829e9792e0f2d02777fce3f743485011ed606c
Mahliyo <gasandigital13@gmail.com>
2026-03-26 21:57:33 +0500

    7-masalani boshladim

--- main.js
+++ main.js
@@ -70,3 +70,13 @@
 //  } else {
 //     alert("false")
 //  }
+
+// 8-masala
+let a = +prompt("a sonini kiriting");
+//  let b = +prompt("b sonini kiriting");
+//  let c = +prompt("c sonini kiriting");
+//  if (a > 0 && b > 0 && c > 0) {
+//     alert("true")
+//  } else {
+//     alert("false")
+//  }
