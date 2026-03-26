fb7bc47d75e26df75fa0391f620a328339dda948
Mahliyo <gasandigital13@gmail.com>
2026-03-26 22:07:27 +0500

    8-masalani yakunladim

--- main.js
+++ main.js
@@ -72,11 +72,11 @@
 //  }
 
 // 8-masala
-let a = +prompt("a sonini kiriting");
- let b = +prompt("b sonini kiriting");
- let c = +prompt("c sonini kiriting");
- if (a > 0 && b > 0 && c > 0) {
-    alert("true")
- } else {
-    alert("false")
- }
+// let a = +prompt("a sonini kiriting");
+//  let b = +prompt("b sonini kiriting");
+//  let c = +prompt("c sonini kiriting");
+//  if ((a > 0 && b <= 0 && c <= 0 ) || (a <= 0 && b > 0 && c <= 0) || (a <= 0 && b <= 0 && c > 0)) {
+//     alert("true")
+//  } else {
+//     alert("false")
+//  }
